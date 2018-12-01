7cc3347eb62485f676a75e38fa049fa5879b8dc2
isobelojalvo <ojalvo@wisc.edu>
2018-12-01 07:40:25 -0600

    2018 PUA development

---
+++ svfitAll.sh
@@ -0,0 +1,39 @@
+ztt_weighted_Nov21_FastMTT
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/tauDATA.root doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ggH125.root  doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/DiBoson.root doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/TT.root      doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WH125.root   doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WJETS.root   doES=1 metType=-1 recoilType=2 isWJets=1 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WmH125.root  doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WpH125.root  doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ZH125.root   doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ZJETS.root   doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ttH125.root  doES=1 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/vbfH125.root doES=1 metType=-1 recoilType=2 
+
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ggH2Jet_maxmix.root    doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ggH2Jet_pseudo.root    doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ggH2Jet_smodel.root    doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/VBFHiggs0L1.root       doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/VBFHiggs0Mf05.root     doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/VBFHiggs0M.root        doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/VBFHiggs0PHf05ph0.root doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/VBFHiggs0PH.root       doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WHiggs0L1fWH05ph0.root doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WHiggs0L1.root         doES=0 metType=-1 recoilType=2 
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WHiggs0MfWH05ph0.root  doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WHiggs0M.root          doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WHiggs0PHfWH05ph0.root doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WHiggs0PH.root         doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/WHiggs0PM.root         doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/Higgs0L1fZH05ph0.root  doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ZHiggs0L1.root         doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ZHiggs0MfZH05ph0.root  doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ZHiggs0M.root          doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ZHiggs0PHfZH05ph0.root doES=0 metType=-1 recoilType=2 
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ZHiggs0PH.root         doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov21_FastMTT/ZHiggs0PM.root         doES=0 metType=-1 recoilType=2 &
+
+#DiBoson.root  TT.root       TT_DL.root    TT_SL.root    TT_had.root   WH125.root    WJETS.root    WmH125.root   WpH125.root   ZH125.root    ZJETS.root    ggH125.root   tauDATA.root  ttH125.root   vbfH125.root
+
---
+++ svfitEmbedded.sh
@@ -0,0 +1,11 @@
+
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov20_FastMTT/ttEmbedded_B.root  doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov20_FastMTT/ttEmbedded_C.root  doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov20_FastMTT/ttEmbedded_D.root  doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov20_FastMTT/ttEmbedded_E.root  doES=0 metType=-1 recoilType=2 &
+FastMTTStandAlonePUATauDM inputfile=/scratch/ojalvo/ztt_weighted_Nov20_FastMTT/ttEmbedded_F.root  doES=0 metType=-1 recoilType=2 &
+
+wait;
+
+#DiBoson.root  TT.root       TT_DL.root    TT_SL.root    TT_had.root   WH125.root    WJETS.root    WmH125.root   WpH125.root   ZH125.root    ZJETS.root    ggH125.root   tauDATA.root  ttH125.root   vbfH125.root
+
